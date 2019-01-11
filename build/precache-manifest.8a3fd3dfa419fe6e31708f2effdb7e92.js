dc317ff746098a6782de40c6ec00b99507588a00
Hollyw00d <matt@mattjennings.net>
2019-01-11 15:50:21 -0800

    added build files

---
+++ build/precache-manifest.8a3fd3dfa419fe6e31708f2effdb7e92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10224e56c171267ece46",
+    "url": "/static/css/main.58f3c02d.chunk.css"
+  },
+  {
+    "revision": "10224e56c171267ece46",
+    "url": "/static/js/main.10224e56.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f85691132775fc7f1d8bcd629a50d201",
+    "url": "/index.html"
+  }
+];
