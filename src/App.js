28ef892e2937382560c3e00adbaa6ffb3722b083
Hollyw00d <matt@mattjennings.net>
2019-01-11 14:38:41 -0800

    trying to get arrays working in react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,47 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
+
+    this.state = {
+      todoItems: []
+    };
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    let todoArr = [];
+
+    this.setState({
+      todoItems: todoArr.push(event.target.value)
+    });
+
+    console.log(this.state);
+  }
+
+  onChange(event) {
+    this.setState({
+      todoItems: event.target.value
+    })
   }
 
   render() {
     return (
       <div id="wrapper">
         <h1>Simple To Do List Using ReactJS</h1>
+
+        <form onSubmit={this.onFormSubmit}>
+          <p>
+            <label htmlFor="todoItem">To Do Item</label><br />
+            <input id="todo-item" name="todoItem" value={this.state.todoItems || ''} onChange={this.onChange} type="text" required minLength="2" maxLength="30" />
+          </p>
+          <p>
+            <input type="submit" value="Add" />
+          </p>
+        </form>
       </div>
     );
   }
