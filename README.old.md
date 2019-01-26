be8cc148f86d937dd5a8e027245856cf8854f303
Hollyw00d <matt@mattjennings.net>
2019-01-25 18:36:11 -0800

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# simple-to-do-list-react
-Simple To Do List using React
---
+++ precache-manifest.8de35a206e8d0e593b511576401558d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b5f84ac169a7c351d65",
+    "url": "/simple-to-do-list-react/static/css/main.baef033c.chunk.css"
+  },
+  {
+    "revision": "2b5f84ac169a7c351d65",
+    "url": "/simple-to-do-list-react/static/js/main.2b5f84ac.chunk.js"
+  },
+  {
+    "revision": "e789722f31e69a497af6",
+    "url": "/simple-to-do-list-react/static/js/1.e789722f.chunk.js"
+  },
+  {
+    "revision": "69de0adce6d7ef269d4c",
+    "url": "/simple-to-do-list-react/static/js/runtime~main.69de0adc.js"
+  },
+  {
+    "revision": "1e2e0912037672f4c5a7377b8f5a2bbb",
+    "url": "/simple-to-do-list-react/index.html"
+  }
+];
